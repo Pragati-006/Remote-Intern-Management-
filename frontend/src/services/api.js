544f7d4bb589f290396b799110dd324dc8b65291
Pragati-006 <pragatisidpara@gmail.com>
2025-08-16 23:29:34 +1000

    api file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+const API = axios.create({ baseURL: '/api' });
 
 // Attach token from localStorage for protected routes
 API.interceptors.request.use((req) => {
